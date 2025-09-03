02f742cbb247ad9e0dd5edfa361dbe04f5123f4f
Aghyad <aaa1997aaa@gmail.com>
2025-09-04 03:54:30 +0400

    add open panel for analytics

--- src/providers/providers.tsx
+++ src/providers/providers.tsx
@@ -2,6 +2,7 @@
 
 import { ThemeProvider as NextThemesProvider } from "next-themes";
 import { ReactNode } from "react";
+import { OpenPanelComponent } from "@openpanel/nextjs";
 
 export function Providers({ children }: { children: ReactNode }) {
   return (
@@ -11,6 +12,14 @@ export function Providers({ children }: { children: ReactNode }) {
       enableSystem
       disableTransitionOnChange
     >
+      <OpenPanelComponent
+        clientId={process.env.NEXT_PUBLIC_OPENPANEL_CLIENT_ID || ""}
+        trackScreenViews={true}
+        // trackAttributes={true}
+        // trackOutgoingLinks={true}
+        // If you have a user id, you can pass it here to identify the user
+        // profileId={'123'}
+      />
       {children}
     </NextThemesProvider>
   );
